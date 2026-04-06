7347a14bee570b771dabc30333b0e4580fb0756c
I Gede Made Devaki Satya Nanda <devakisatyananda@gmail.com>
2026-04-07 01:06:23 +0700

    Menambahkan Quiz Screen dan logic soal

---
+++ app/src/main/java/com/devakisatya0003/assessment/ui/screen/QuizScreen.kt
@@ -0,0 +1,76 @@
+package com.devakisatya0003.assessment.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.devakisatya0003.assessment.R
+
+@Composable
+fun QuizScreen(navController: NavHostController, name: String) {
+
+    var index by rememberSaveable { mutableIntStateOf(0) }
+    var score by rememberSaveable { mutableIntStateOf(0) }
+    var selected by rememberSaveable { mutableIntStateOf(-1) }
+
+    val question = questionList[index]
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        Text(
+            stringResource(
+                R.string.question_number,
+                index + 1,
+                questionList.size
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Image(
+            painter = painterResource(question.imageRes),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(question.question)
+
+        question.options.forEachIndexed { i, option ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(
+                    selected = selected == i,
+                    onClick = { selected = i }
+                )
+                Text(option)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+
+            if (selected == question.answer) {
+                score += 10
+            }
+
+            if (index < questionList.size - 1) {
+                index++
+                selected = -1
+            } else {
+                navController.navigate("result/$score")
+            }
+
+        }) {
+            Text(stringResource(R.string.btn_next))
+        }
+    }
+}
